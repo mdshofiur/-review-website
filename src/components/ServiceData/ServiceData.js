6d7e7b29c52a2dde9b9d565a6f3ebca27334efa6
mdshofiur <shofiurbd13@gmail.com>
2021-10-04 14:57:22 +0600

    added

---
+++ src/components/ServiceData/ServiceData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useState,useEffect} from 'react'
+
+
+const ServiceData = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ServiceData;
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//./Hotel.JSON
 
 const Services = () => {
     return (
